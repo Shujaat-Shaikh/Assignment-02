b79bd9f1b4c70fab0b5ba2a247f27b43e6165ec0
Shujaat-Shaikh <100413587+Shujaat-Shaikh@users.noreply.github.com>
2022-03-15 14:38:22 +0500

    Add files via upload

---
+++ 8thAssignment.dart
@@ -0,0 +1,16 @@
+void main(){
+  
+  
+  var a = 10; //"Dollar"); 
+  var b = 25; //"Saudi Riyal";
+  var c = 175.7; //"Dollar to Pakistani Rupees";
+  var d = 46.65; //"Saudi Riyal to Pakistani Rupees";
+  // var e = c * a;
+  // var f = b * d;
+// print("Exchange rate of Dollar ${a%c}");
+// print("Exchange rate of Saudi Riyal ${b%d}");
+print("Exchange rate of 1 Dollar = 175.70 Pakistani Rupee = ${c * a} Pakistani Rupee");
+print("Exchange rate of 1 Saudi Riyal = 46.65 Pakistani Rupee = ${b * d} Pakistani Rupee");
+
+    
+}
---
+++ 9thAssignment.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+void main(){
+
+  stdout.write("Enter Working Hours ");
+  var a = int.parse(stdin.readLineSync()!);
+  var b = 100;
+  var c = a* b;
+
+  print("Working Hour:${a}");
+  print("Total pay = ${c}");
+
+
+
+
+
+
+
+    
+}
